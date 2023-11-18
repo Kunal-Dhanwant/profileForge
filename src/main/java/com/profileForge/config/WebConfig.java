b0057f0511f53c57635114918b5385590e1a7fa2
Kunal Dhanwant <kunaldhanwant8851@gmail.com>
2023-11-18 18:18:41 +0700

    cors config added

--- src/main/java/com/profileForge/config/WebConfig.java
+++ src/main/java/com/profileForge/config/WebConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+
+
+
 public class WebConfig  {
 
     @Bean
