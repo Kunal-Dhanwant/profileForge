1a76ea3d5d0b9435f6e863a53b44ecdadf51156c
Kunal Dhanwant <kunaldhanwant8851@gmail.com>
2023-11-18 18:02:09 +0700

    add cors origin deploy

--- src/main/java/com/profileForge/config/WebConfig.java
+++ src/main/java/com/profileForge/config/WebConfig.java
@@ -7,10 +7,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
-
-
-
 public class WebConfig  {
 
     @Bean
